169a1e10e1bf930dca043bdde3d87e7d9a699300
Dipam <59444569+dipamsen@users.noreply.github.com>
2020-03-25 23:35:26 +0530

    uploaded files

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var sball;
+var position;
+var database;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    sball = createSprite(250,250,10,10);
+    sball.shapeColor = "red";
+    var ballRef = database.ref('ball/position');
+    ballRef.on("value", readPosition, showError);
+}
+
+function readPosition(data){
+    position = data.val();
+    sball.x = position.x;
+    sball.y= position.y;
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            changePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            changePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            changePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            changePosition(0,+1);
+        }    
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    });
+}
+
+function showError(){
+    console.log("ERROR");
+}
